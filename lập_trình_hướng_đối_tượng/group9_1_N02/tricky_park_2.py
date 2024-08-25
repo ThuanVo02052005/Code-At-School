6c6d6b15a7e967fa1d5fba0e8ef0a9dc7faba8e1
ThuanVo02052005 <23010427@st.phenikaa-uni.edu.vn>
2024-08-25 22:39:52 +0700

    updeate

--- lập_trình_hướng_đối_tượng/group9_1_N02/tricky_park_2.py
+++ lập_trình_hướng_đối_tượng/group9_1_N02/tricky_park_2.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
     n1.i = 2 
     n2.i = 5 
     n1 = n2 
+    print(f"giá trị của n1.i : {n1.i}")
     n2.i = 10 
     print(f"giá trị của n1.i : {n1.i}")
+    print(f"giá trị của n2.i: {n2.i}")
     n1.i = 20 
     print(f"giá trị của n2.i: {n2.i}")
     print(f"giá trị của n1.i: {n1.i}")
